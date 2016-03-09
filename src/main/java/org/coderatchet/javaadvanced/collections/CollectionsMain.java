96ab417729837027c0c90bb3ff147cea71fefaa7
coderatchet <jared@thenaglecode.com>
2016-03-09 12:34:09 +1100

    collection and list examples

---
+++ src/main/java/org/coderatchet/javaadvanced/collections/CollectionsMain.java
@@ -0,0 +1,43 @@
+package org.coderatchet.javaadvanced.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionsMain {
+	public static void main(String[] args) {
+		collectionExample();
+		listExample();
+	}
+	
+	public static void collectionExample() {
+		Collection<String> collection = new ArrayList<String>();
+		collection.add("something1");
+		collection.add("something2");
+		collection.add("something3");
+		collection.add("something4");
+		collection.add("something5");
+		collection.add("something6");
+		// collection.get(index) not avaiable
+		collection.forEach(str -> System.out.println(str));
+	}
+	
+	public static void listExample() {
+		List<String> list = new ArrayList<String>();
+		list.add("lalala 6");
+		list.add("lalala 5");
+		list.add("lalala 4");
+		list.add("lalala 3");
+		list.add("lalala 2");
+		list.add("lalala 1");
+		
+		list.remove(4);
+
+		System.out.println("item number 2 is: " + list.get(2));
+		try {
+			System.out.println("item number 5 is: " + list.get(5));
+		} catch (Exception e) {
+			System.out.println("5 is out of bounds! remember? I removed one item");
+		}
+	}
+}
