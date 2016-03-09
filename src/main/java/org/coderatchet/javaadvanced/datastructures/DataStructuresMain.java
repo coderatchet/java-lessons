39f58ce6874c990262b37ac4fec5509fb8ff225a
coderatchet <jared@thenaglecode.com>
2016-03-09 11:45:05 +1100

    bitset example

--- src/main/java/org/coderatchet/javaadvanced/datastructures/DataStructuresMain.java
+++ src/main/java/org/coderatchet/javaadvanced/datastructures/DataStructuresMain.java
@@ -1,15 +1,17 @@
 package org.coderatchet.javaadvanced.datastructures;
 
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Enumeration;
 import java.util.Vector;
 
 public class DataStructuresMain {
 	public static void main(String[] args) {
-		daysOfWeekExample();
+		enumExample();
+		bitSetExample();
 	}
 	
-	public static void daysOfWeekExample() {
+	public static void enumExample() {
 		for(DaysOfWeek day: DaysOfWeek.values()) {
 			System.out.println(day + " will be sunny!");
 		}
@@ -19,5 +21,14 @@ public class DataStructuresMain {
 		while(itr.hasMoreElements()) {
 			System.out.println("  * " + itr.nextElement());
 		}
+	}
+	
+	public static void bitSetExample() {
+		BitSet myFavoriteBits = new BitSet(8);
+		myFavoriteBits.set(0);
+		myFavoriteBits.set(3);
+		myFavoriteBits.or(BitSet.valueOf(new byte[] {0b01010101})); // binary literal
+		
+		System.out.println("my favorite bits are: " + myFavoriteBits);
 	}
 }
